85f8419d2bf9c4a5d22a65f06d489680ab4d1644
tonyyangmckinsey <tony_yang@mckinsey.com>
2025-05-20 23:13:03 +0200

    says

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from compute_initial_unique_crids import compute_unique_crids
 from compute_initial_solution import compute_initial_solution
-from add_initial_new_orders import add_new_orders
+from add_initial_new_orders import add_initial_new_orders
+from update_solution_from_snapshots import update_solution_from_snapshots
 
 
 def main():
@@ -11,8 +12,10 @@ def main():
     print("2. Construct initial solution (No more use)")
     #compute_initial_solution()
 
-    print("3. Add new orders from initial snapshot (No more use)")
+    print("3. Add new orders from initial snapshot (No more use after Wednesday)")
     add_initial_new_orders()
+
+    print("4. add new orders from future snapshots")
 
 
 
