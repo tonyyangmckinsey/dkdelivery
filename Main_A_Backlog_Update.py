ad9ee366b8a7aa85bd11d7d06dab5980d4378a8c
tonyyangmckinsey <tony_yang@mckinsey.com>
2025-05-22 15:11:10 +0200

    says

--- Main_A_Backlog_Update.py
+++ Main_A_Backlog_Update.py
@@ -1,8 +1,8 @@
 from A_A_compute_initial_unique_crids import compute_unique_crids
 from A_B_compute_initial_solution import compute_initial_solution
 from A_C_add_initial_new_orders import add_initial_new_orders
-from A_D_update_solution_from_snapshots import update_solution_from_snapshots
-from consolidate_heatmap_performance_sheets import consolidate_performance_sheets
+from Main_AA_Produce_Order_Development_Output import update_solution_from_snapshots
+from Main_C_consolidate_heatmap_performance_sheets import consolidate_performance_sheets
 
 
 def main():
@@ -13,10 +13,10 @@ def main():
     print("2. Construct initial solution (No more use)")
     #compute_initial_solution()
 
-    print("3. Add new orders from initial snapshot (No more use after Wednesday)")
+    print("3. Add new orders from initial snapshot (No more use after Wednesday)(No more use)")
     #add_initial_new_orders()
 
-    print("4. add new orders from future snapshots")
+    print("4. add new orders from future snapshots to produce Order_Devleopment_Output (Main algo)")
     #update_solution_from_snapshots()
 
     #print("Consolidate heatmaps")
